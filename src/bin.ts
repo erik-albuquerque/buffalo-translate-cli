55694d65ab803f6b5219d1b05da9a6da2233cae1
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-09-25 17:20:45 -0300

    rename program to command

--- src/bin.ts
+++ src/bin.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-import { program } from './command'
+import { command } from './command'
 
-program.parse()
+command.parse()
--- src/command.ts
+++ src/command.ts
@@ -1,14 +1,16 @@
 import { Command } from 'commander'
 
-export const program = new Command()
+const command = new Command()
 
-program
+command
   .name('buffalo-translate-cli')
   .description('Buffalo Translate CLI offers the ability to perform translations directly in the terminal, providing an efficient way of translating texts from one language to another.')
   .version('1.0.1', '-v, --version', 'output the current version')
 
-program
+command
   .configureHelp({
     sortSubcommands: true,
     subcommandTerm: (cmd) => cmd.name()
   })
+
+export { command }
