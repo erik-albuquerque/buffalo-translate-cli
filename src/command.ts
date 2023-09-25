5fc5e3c932ed968f14193ec0694af63c5808f35d
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-09-25 18:04:02 -0300

    setup prettier, eslint, editorconfig and .vscode/settings

--- src/command.ts
+++ src/command.ts
@@ -4,13 +4,14 @@ const command = new Command()
 
 command
   .name('buffalo-translate-cli')
-  .description('Buffalo Translate CLI offers the ability to perform translations directly in the terminal, providing an efficient way of translating texts from one language to another.')
+  .description(
+    'Buffalo Translate CLI offers the ability to perform translations directly in the terminal, providing an efficient way of translating texts from one language to another.'
+  )
   .version('1.0.1', '-v, --version', 'output the current version')
 
-command
-  .configureHelp({
-    sortSubcommands: true,
-    subcommandTerm: (cmd) => cmd.name()
-  })
+command.configureHelp({
+  sortSubcommands: true,
+  subcommandTerm: (cmd) => cmd.name()
+})
 
 export { command }
