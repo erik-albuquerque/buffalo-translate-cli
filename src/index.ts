d1ce5d3f409f110e3c05728e273a38755fe0dddf
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-09-25 12:20:06 -0300

    setup help command

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,12 @@ program
   .name('buffalo-translate-cli')
   .description('Buffalo Translate CLI offers the ability to perform translations directly in the terminal, providing an efficient way of translating texts from one language to another.')
   .version('1.0.0', '-v, --version', 'output the current version')
+
+program
+  .configureHelp({
+    sortSubcommands: true,
+    subcommandTerm: (cmd) => cmd.name()
+  })
+
+program
+  .parse()
